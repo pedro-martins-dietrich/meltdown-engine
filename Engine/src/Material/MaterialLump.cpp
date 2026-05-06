5ddb90ae5b3fb19e95dc3073706534abbea37e49
Pedro Martins Dietrich <pedro.martins.dietrich@gmail.com>
2026-05-07 02:20:38 +0900

    Add custom string literal to uint operator
    This user defined literals are useful for creating or reading magic
    words at the beginning of certain file types.

--- Engine/src/Material/MaterialLump.cpp
+++ Engine/src/Material/MaterialLump.cpp
@@ -2,13 +2,10 @@
 #include "MaterialLump.hpp"
 
 mtd::MaterialLump::MaterialLump(const Device& mtdDevice, const MaterialInfo& materialInfo)
-	: mtdDevice{mtdDevice},
-	materialInfo{materialInfo},
-	floatDataBuffer{mtdDevice, vk::BufferUsageFlagBits::eStorageBuffer, vk::MemoryPropertyFlagBits::eDeviceLocal},
-	materialCount{0U}
+	: mtdDevice{mtdDevice}, materialInfo{materialInfo},
+	floatDataBuffer{mtdDevice, vk::BufferUsageFlagBits::eStorageBuffer, vk::MemoryPropertyFlagBits::eDeviceLocal}
 {}
 
-// Loads material float data and textures
 void mtd::MaterialLump::addMaterial(const float* data, size_t size, const std::vector<std::string>& texturePaths)
 {
 	floatMaterialAttributes.insert(floatMaterialAttributes.cend(), data, data + size);
@@ -25,7 +22,6 @@ void mtd::MaterialLump::addMaterial(const float* data, size_t size, const std::v
 	materialCount++;
 }
 
-// Assigns the material float data buffer as a descriptor
 void mtd::MaterialLump::assignFloatDataBufferToDescriptor
 (
 	DescriptorSetHandler& descriptorSetHandler, uint32_t swappableSetIndex, uint32_t bindingIndex
@@ -34,7 +30,6 @@ void mtd::MaterialLump::assignFloatDataBufferToDescriptor
 	descriptorSetHandler.assignExternalResourcesToDescriptor(swappableSetIndex, bindingIndex, floatDataBuffer);
 }
 
-// Assigns the material textures to a descriptor
 void mtd::MaterialLump::assignTexturesToDescriptor
 (
 	DescriptorSetHandler& descriptorSetHandler, uint32_t swappableSetIndex, uint32_t bindingIndex
@@ -47,7 +42,6 @@ void mtd::MaterialLump::assignTexturesToDescriptor
 	descriptorSetHandler.createImagesDescriptorResources(swappableSetIndex, bindingIndex, descriptorImageInfos);
 }
 
-// Loads all materials to the GPU VRAM
 void mtd::MaterialLump::loadMaterialsToGPU(const CommandHandler& commandHandler)
 {
 	floatDataBuffer.createDeviceLocal
--- Engine/src/Utils/StringParser.hpp
+++ Engine/src/Utils/StringParser.hpp
@@ -3,9 +3,36 @@
 #include <string>
 #include <vector>
 
-// Common string manipulation functions
-namespace mtd::StringParser
+namespace mtd
 {
-	// Separates a line into substrings, using the specified delimiter
-	void split(std::string line, std::string delimiter, std::vector<std::string>& output);
+	// Common string manipulation functions
+	namespace StringParser
+	{
+		// Separates a line into substrings, using the specified delimiter
+		void split(std::string line, std::string delimiter, std::vector<std::string>& output);
+	}
+
+	// Converts an 4-character string literal to an uint32_t
+	constexpr uint32_t operator""_u32(const char* str, size_t size)
+	{
+		if(size != sizeof(uint32_t))
+			throw "Literal must contain exactly 4 characters.";
+
+		uint32_t value = 0U;
+		for(size_t i = 0; i < size; i++)
+			value |= static_cast<uint32_t>(str[i]) << (8 * i);
+		return value;
+	}
+
+	// Converts an 8-character string literal to an uint64_t
+	constexpr uint64_t operator""_u64(const char* str, size_t size)
+	{
+		if(size != sizeof(uint64_t))
+			throw "Literal must contain exactly 8 characters.";
+
+		uint64_t value = 0UL;
+		for(size_t i = 0; i < size; i++)
+			value |= static_cast<uint64_t>(str[i]) << (8 * i);
+		return value;
+	}
 }
